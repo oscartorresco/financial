f4295a07aaa9c24a4cbfcf87de9e9c8de8facf0c
shedmanchavez <shedman9.2@gmail.com>
2013-10-22 17:40:46 -0500

    Update models.py

--- financiero/compras/models.py
+++ financiero/compras/models.py
@@ -1,39 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Bodega(models.Model):
-	codigo = models.IntegerField(primary_key=True)
-	nombre = models.CharField(max_length=100,null=False)
-	direccion = models.CharField(max_length=100,null=False)
-	def __unicode__(self):
-		return self.nombre+' Ubicada en '+self.direccion
-
 class Compra(models.Model):
 	codigo = models.IntegerField(primary_key=True)
 	descripcion = models.CharField(max_length=100,null=False)
 	fecha = models.DateField(null=False)
 	def __unicode__(self):
 		return self.descripcion
-
-class Producto(models.Model):
-    codigo = models.IntegerField(primary_key=True, null=False)
-    nombre = models.CharField(max_length=100, null=False)
-    descripcion = models.CharField(max_length=300, null=False)
-    UNIDADES_MEDIDA =(
-    		('Unidad', 'Unidad'),
-    		('Decena', 'Decenas'),
-    		('Docena', 'Docenas'),
-    		('Kg','Kilogramo'),
-    		('gr','Gramo'),
-    		('m','Metro')
-    	)
-    medida = models.CharField(max_length=30, null=False, choices=UNIDADES_MEDIDA)
-    inventario_minimo = models.IntegerField(null=False)
-    cantidad = models.IntegerField(null=False)
-    fecha_ingreso = models.DateField(null=False)
-    bodega = models.ForeignKey(Bodega)
-    def __unicode__(self):
-		return self.nombre
 
 class Compra_Producto(models.Model):
 	cantidad = models.IntegerField(null=False)
