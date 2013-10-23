71470ef6a22ee2dd9de0688a516e9b60fe78fb99
Oscar Torres Corredor <oscartorresco@hotmail.com>
2013-10-23 16:18:16 -0500

    Inventario

--- financiero/inventory/views.py
+++ financiero/inventory/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 from inventory.models import Bodega, Producto
+from inventory.forms import ProductForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
 from datetime import *
 
 def create_product(request):
@@ -14,14 +16,15 @@ def create_product(request):
 		minimo = request.POST['minimo'];
 		cantidad = request.POST['cantidad'];
 		bodega = request.POST['bodega'];
-		time=datetime.today()
-		format=time.strftime('%Y-%m-%d %H:%M:%S')
-		bodega = Bodega.objects.all();
-		Producto.objects.create(nombre=nombre, descripcion=descripcion, medida=medida, inventario_minimo=minimo, fecha_ingreso=format, bodega_id=bodega);
-		return HttpResponseRedirect('/products')
+		image = request.POST['imagen'];
+		time = datetime.today()
+		format = time.strftime('%Y-%m-%d')
+		Producto.objects.create(nombre=nombre, descripcion=descripcion, medida=medida, inventario_minimo=minimo, cantidad=cantidad, fecha_ingreso=format, bodega_id=bodega);
+		photo = impleUploadedFile('imagencliente.jpg', buf.getvalue(), "image/jpeg")
+		return HttpResponseRedirect('/cellars')
 	else:
-		product = Producto.objects.all();
-		return render_to_response('create_product.html', context_instance=RequestContext(request));
+		bodegas = Bodega.objects.all()
+		return render_to_response('create_product.html',{'bodegas': bodegas}, context_instance=RequestContext(request))
 
 def create_cellar(request):
 	if request.method == 'POST':
@@ -30,11 +33,20 @@ def create_cellar(request):
 		Bodega.objects.create(nombre=nombre, direccion=direccion);
 		return HttpResponseRedirect('/cellars')
 	else:
-		bodega = Bodega.objects.all();
-		return render_to_response('create_cellar.html', context_instance=RequestContext(request));
+		bodegas = Bodega.objects.all()
+		return render_to_response('create_cellar.html', context_instance=RequestContext(request))
 
 def table_cellar(request):
-	cellar = Bodega.objects.all();
-	return render_to_response('cellars.html',{'cellar':cellar})
+	bodegas = Bodega.objects.all()
+	return render_to_response('cellars.html',{'bodegas': bodegas}, context_instance=RequestContext(request))
 
+def example(request):
+    if request.method=='POST':
+        formulario = ProductForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/cellars')
+    else:
+        formulario = ProductForm()
+    	return render_to_response('example.html',{'formulario':formulario}, context_instance=RequestContext(request))
 
