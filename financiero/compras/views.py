04f9c07c841d29e42ac10576e08a7abdabf13222
Shedman Chavez <shedman9.2@gmail.com>
2013-10-23 19:59:01 -0500

    Actualizacion modulo compras

--- financiero/compras/views.py
+++ financiero/compras/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.http import HttpResponse
 from compras.models import Proveedor, Producto_Proveedor, Compra
+from inventory.models import Producto
 
 #Vistas de Compras
 def indexCompras(request):
