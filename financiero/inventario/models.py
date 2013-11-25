a684f34119ba91cacedce65a30c70bd14b66c1ac
shedmanchavez <shedman9.2@gmail.com>
2013-11-25 17:35:46 -0500

    Cambios compras
    Agregue unas class en el models de inventario para que funcione
    correctamente

--- financiero/inventario/models.py
+++ financiero/inventario/models.py
@@ -1,16 +1,21 @@
-#encoding:utf-8
+﻿#encoding:utf-8
 from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
-class  Bodega(models.Model):
+class Bodega(models.Model):
 	codigo = models.AutoField(max_length=30, primary_key=True)
 	nombre = models.CharField(max_length=30)
 	direccion = models.CharField(max_length=50)
 	def __unicode__(self):
 		return self.nombre
 
-class  Producto(models.Model):
+class ProductoManager(models.Manager):
+	def get_by_natural_key(self, nombre):
+		return self.get(nombre = nombre)
+
+class Producto(models.Model):
+	objects = ProductoManager()
 	codigo = models.AutoField(max_length=30, primary_key=True)
 	nombre = models.CharField(max_length=30)
 	descripcion = models.CharField(max_length=50)
@@ -19,7 +24,7 @@ class  Producto(models.Model):
 	cantidad = models.IntegerField(max_length=30)
 	fecha_ingreso = models.DateField(auto_now=False)
 	estado = models.BooleanField(null=False)
-	imagen = models.ImageField(upload_to='financial', verbose_name='Imágen')
+	imagen = models.ImageField(upload_to='financial', verbose_name='Imagen')
 	bodega =models.ForeignKey(Bodega)
 	def __unicode__(self):
 		return self.bodega
